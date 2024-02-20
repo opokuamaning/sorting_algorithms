71ec89b64ac77e917a66175da15ab5455a13a74a
opokuamaning <princeopokuamaning@gmail.com>
2024-02-20 07:36:05 -0800

    Added counting_sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,51 +1,51 @@
 #include "sort.h"
+
 /**
- * counting_sort - counting sort algo
- * @array: array to sort
- * @size: size of array
+ * counting_sort - Algorithm that sorts the array in order ascended
+ *@array: type array of integers.
+ *@size: type size of array.
  */
+
 void counting_sort(int *array, size_t size)
 {
+	int *count, *sort, sum, mx;
 	size_t i;
-	int j, k, num, dup;
-	int *counts;
 
 	if (array == NULL || size < 2)
 		return;
-	k = array[0]; /* find max num to malloc size of new array */
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > k)
-			k = array[i];
-	}
-	counts = malloc(sizeof(int) * (k + 1));
-	if (counts == NULL)
-		return;
-	for (j = 0; j < (k + 1); j++) /* memset counts array to 0 */
-		counts[j] = 0;
-	for (i = 0; i < size; i++) /* input counts */
-	{
-		num = array[i];
-		counts[num] += 1;
-	}
-	for (j = 0; j < k; j++) /* update counts array */
-	{
-		counts[j + 1] += counts[j];
-	}
-	print_array(counts, k + 1);
-	for (i = 0, j = 0; j < k; j++) /* replace array with sorted */
-	{
-		if ((j == 0) && counts[j] != 0)
-		{
-			for ((dup = counts[j]); dup > 0; dup--)
-				array[i++] = j;
-		}
-		if (counts[j + 1] > counts[j])
-		{
-			for ((dup = counts[j + 1] - counts[j]); dup > 0; dup--)
-				array[i++] = (j + 1);
-		}
-	}
 
-	free(counts);
+	sort = malloc(sizeof(int) * size);
+	if (sort == NULL)
+		return;
+
+	mx = array[0];
+	for (i = 1; i < size; i++)
+		mx = (array[i] > mx) ? array[i] : mx;
+	count = malloc(sizeof(int) * (mx + 1));
+	if (count == NULL)
+		return;
+
+	for (i = 0; i < (size_t)(mx + 1); i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	for (i = 0, sum = 0; i < (size_t)(mx + 1); i++)
+	{
+		count[i] += sum;
+		sum = count[i];
+	}
+	print_array(count, mx + 1);
+
+	for (i = 0; i < size; i++)
+	{
+		sort[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = sort[i];
+
+	free(sort);
+	free(count);
 }
